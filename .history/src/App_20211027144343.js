8532a2f2e6304ae51453be983861ec7cf47620c8
tai <nguyentientai10@gmail.com>
2021-10-28 14:36:17 +0700

    Start setting

--- .history/src/App_20211027144343.js
+++ .history/src/App_20211027144343.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
